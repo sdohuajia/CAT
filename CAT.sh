1a3f3be8975f5c52e3783b0be893db6cb939c369
sdohuajia <63113882+sdohuajia@users.noreply.github.com>
2024-09-12 00:00:42 +0800

    Update CAT.sh

--- CAT.sh
+++ CAT.sh
@@ -12,7 +12,7 @@ fi
 
 # 安装 Docker
 install_docker() {
-    echo "正在安装 Docker..."
+    echo "开始安装 Docker..."
     apt-get update -q && apt-get upgrade -yq
     apt-get install -yq apt-transport-https ca-certificates curl software-properties-common
 
@@ -29,7 +29,7 @@ install_docker() {
 
 # 安装 Node.js
 install_node() {
-    echo "正在安装 Node.js..."
+    echo "开始安装 Node.js..."
     apt-get update -q
     curl -fsSL https://deb.nodesource.com/setup_16.x | bash -
     apt-get install -yq nodejs
@@ -39,8 +39,8 @@ install_node() {
 
 # 安装 Docker Compose
 install_docker_compose() {
-    echo "正在安装 Docker Compose..."
     local version="2.20.2"
+    echo "开始安装 Docker Compose (版本 $version)..."
     curl -L "https://github.com/docker/compose/releases/download/v$version/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
     chmod +x /usr/local/bin/docker-compose
 
@@ -49,10 +49,9 @@ install_docker_compose() {
 
 # 查看同步日志和余额
 check_node_log() {
-    echo "正在查看同步日志和余额..."
+    echo "查看同步日志和余额..."
     yarn cli wallet balances
     
-    # 提示用户按任意键返回主菜单
     echo "按任意键返回主菜单..."
     read -n 1 -s
 }
@@ -71,17 +70,15 @@ export_wallet_info() {
     echo "Name: $(grep -oP '"name": *"\K[^"]+' "$wallet_file")"
     echo "Mnemonic: $(grep -oP '"mnemonic": *"\K[^"]+' "$wallet_file")"
 
-    # 提示用户按任意键返回主菜单
     echo "按任意键返回主菜单..."
     read -n 1 -s
 }
 
 # 执行 mint
 execute_mint() {
-    echo "正在执行 mint 操作..."
+    echo "执行 mint 操作..."
     ./script.sh
 
-    # 提示用户按任意键返回主菜单
     echo "按任意键返回主菜单..."
     read -n 1 -s
 }
@@ -92,76 +89,69 @@ display_address() {
     cd /root/cat-token-box/packages/cli || { echo "进入目录失败"; exit 1; }
     yarn cli wallet address
 
-    # 提示用户按任意键返回主菜单
     echo "按任意键返回主菜单..."
     read -n 1 -s
 }
 
 # 创建钱包
 create_wallet() {
-    echo "正在创建钱包..."
+    echo "创建钱包..."
     cd /root/cat-token-box/packages/cli || { echo "进入目录失败"; exit 1; }
     yarn cli wallet create
+
+    echo "按任意键返回主菜单..."
+    read -n 1 -s
 }
 
 # 安装节点
 install_dependencies() {
     echo "开始安装节点..."
     
-    # 检查 Docker 是否已安装
     if ! command -v docker &> /dev/null; then
         install_docker
     else
         echo "Docker 已安装，跳过安装步骤。"
     fi
 
-    # 检查 Node.js 是否已安装
     if ! command -v node &> /dev/null; then
         install_node
     else
         echo "Node.js 已安装，跳过安装步骤。"
     fi
 
-    # 检查 Docker Compose 是否已安装
     if ! command -v docker-compose &> /dev/null; then
         install_docker_compose
     else
         echo "Docker Compose 已安装，跳过安装步骤。"
     fi
 
-    # 输出 Docker 和 Docker Compose 版本
     echo "Docker 状态:"
     systemctl status docker --no-pager
 
     echo "Docker Compose 版本:"
     docker-compose --version
 
-    # 拉取 GitHub 仓库并构建项目
-    echo "正在克隆 GitHub 仓库..."
+    echo "克隆 GitHub 仓库..."
     git clone https://github.com/CATProtocol/cat-token-box.git || { echo "克隆失败"; exit 1; }
     cd cat-token-box/ || { echo "进入目录失败"; exit 1; }
 
     echo "安装依赖并构建项目..."
     yarn install && yarn build
 
-    # 进入 tracker 目录并设置权限
     cd packages/tracker/ || { echo "进入 tracker 目录失败"; exit 1; }
     echo "设置权限..."
     mkdir -p docker/data docker/pgdata
     chmod 777 docker/data
     chmod 777 docker/pgdata
 
-    # 启动 Docker Compose
     echo "启动 Docker Compose..."
     docker-compose up -d
 
-    # 构建 Docker 镜像
     cd ../..
-    echo "正在构建 Docker 镜像..."
+    echo "构建 Docker 镜像..."
     docker build -t tracker:latest .
 
-    # 运行 Docker 容器
-    echo "正在运行 Docker 容器..."
+    echo "运行 Docker 容器..."
     docker run -d \
         --name tracker \
         --add-host="host.docker.internal:host-gateway" \
@@ -170,13 +160,11 @@ install_dependencies() {
         -p 3000:3000 \
         tracker:latest
 
-    # 进入特定目录
     cd packages/cli || { echo "进入 CLI 目录失败"; exit 1; }
     echo "已进入目录 /root/cat-token-box/packages/cli"
 
-    # 自动写入 config.json
-    echo "正在创建或修改 config.json 文件..."
-    tee config.json > /dev/null <<EOF
+    echo "创建或修改 config.json 文件..."
+    cat <<EOF > config.json
 {
   "network": "fractal-mainnet",
   "tracker": "http://127.0.0.1:3000",
@@ -190,9 +178,8 @@ install_dependencies() {
 }
 EOF
 
-    # 创建并写入 script.sh 文件
-    echo "正在创建并写入 script.sh 文件..."
-    tee script.sh > /dev/null <<EOF
+    echo "创建并写入 script.sh 文件..."
+    cat <<EOF > script.sh
 #!/bin/bash
 
 command="yarn cli mint -i 45ee725c2c5993b3e4d308842d87e973bf1951f5f7a804b21e4dd964ecd12d6b_0 5"
@@ -209,11 +196,8 @@ while true; do
 done
 EOF
 
-    # 给予 script.sh 执行权限
-    echo "正在赋予 script.sh 执行权限..."
     chmod +x script.sh
 
-    # 提示用户按任意键返回主菜单
     echo "按任意键返回主菜单..."
     read -n 1 -s
 }
@@ -228,11 +212,12 @@ main_menu() {
         echo "退出脚本，请按键盘 ctrl + C 退出即可"
         echo "请选择要执行的操作:"
         echo "1) 安装节点"
-        echo "2) 导出钱包"
-        echo "3) 执行 mint"
-        echo "4) 查看同步日志和余额"
-        echo "5) 显示地址"
-        echo "6) 退出"
+        echo "2) 创建钱包"
+        echo "3) 导出钱包"
+        echo "4) 执行 mint"
+        echo "5) 查看同步日志和余额"
+        echo "6) 显示地址"
+        echo "7) 退出"
 
         read -p "请输入选项: " option
 
@@ -241,18 +226,21 @@ main_menu() {
                 install_dependencies
                 ;;
             2)
-                export_wallet_info
+                create_wallet
                 ;;
             3)
-                execute_mint
+                export_wallet_info
                 ;;
             4)
-                check_node_log
+                execute_mint
                 ;;
             5)
-                display_address
+                check_node_log
                 ;;
             6)
+                display_address
+                ;;
+            7)
                 echo "退出脚本。"
                 exit 0
                 ;;
